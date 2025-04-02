e3e40594f537839b6ab35bc575cd0dac15eb4c02
Lauren <lsharp22@charlotte.edu>
2025-04-02 10:35:46 -0400

    base views, templates and urls

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('calendarSync/', views.calendarSync, name="calendarSync"),
+    path('courseLoad/', views.courseLoad, name="courseLoad"),
+    path('roadMap/', views.roadMap, name="roadMap"),
+    path('waitListClearance', views.waitListClearance, name="waitListClearance"),
+    path('openSeat/', views.openSeat, name="openSeat"),
+    path('sectionRec/', views.sectionRec, name="sectionRec"),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def calendarSync(request):
+    return render(request, 'base/calendarSync.html')
+def courseLoad(request):
+    return render(request, 'base/courseLoad.html')
+def roadMap(request):
+    return render(request, 'base/roadMap.html')
+def waitListClearance(request):
+    return render(request, 'base/waitListClearance.html')
+def openSeat(request):
+    return render(request, 'base/openSeat.html')
+def sectionRec(request):
+    return render(request, 'base/sectionRec.html')
