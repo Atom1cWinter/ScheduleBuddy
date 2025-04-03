5e4ec6b08f82abfa7254b9fd029e2f02c5a1b980
Lauren <lsharp22@charlotte.edu>
2025-04-03 08:39:18 -0400

    added profile forms, model, and set up signals

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True)  
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
