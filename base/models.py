1af515f629921d6898c6b0604fd6558556de6a88
Atom1cWinter <minorwinter29@gmail.com>
2025-04-11 16:42:40 -0400

    Accidently created a second base folder. Corrected issue

--- base/models.py
+++ base/models.py
@@ -12,3 +12,31 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# Model for course template
+# params:
+# course title, number, professor, start date, meeting dates, meeting times, class type
+
+class Course(models.Model): 
+    title = models.CharField(max_length=200)
+    course_number = models.CharField(max_length=50)
+    professor = models.CharField(max_length=100)
+    start_date = models.DateField() # Dates saved as YYYY-MM-DD
+    meeting_dates = models.CharField(
+        max_length=20, 
+        choices=[
+            ('Monday', 'Monday'),
+            ('Tuesday', 'Tuesday'),
+            ('Wednesday', 'Wednesday'),
+            ('Thursday', 'Thursday'),
+            ('Friday', 'Friday'),
+            ('Saturday', 'Saturday'),
+            ('Sunday', 'Sunday'),
+            ]) 
+    meeting_times = models.TimeField() # Time is saved in HH:MM:SS format
+    end_date = models.DateField() # YYYY-MM-DD
+    class_type = models.CharField(max_length=50, choices=[('In-Class', 'In-Class'), ('Virtual', 'Virtual'), ('Mixed', 'Mixed')])
+
+    def __str__(self):
+        return f"{self.title} ({self.course_number})"
