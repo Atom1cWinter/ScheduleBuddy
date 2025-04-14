b79e4bf943a292360637004b857cfa0407c6f3e5
Atom1cWinter <minorwinter29@gmail.com>
2025-04-14 14:01:08 -0400

    Added course functions to urlpatterns

--- schedulebuddy/urls.py
+++ schedulebuddy/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 
@@ -9,4 +10,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('add_course/', views.add_course, name='add_course'),
+    path('get_courses/', views.get_courses, name='get_courses')
+    
 ]
